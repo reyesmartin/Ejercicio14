13b284d001ea6530b584dc2bfcb3e3b5bc6e060f
Miguel Reyes <mreyesma@indra.es>
2023-05-11 13:22:56 +0200

    Cambios

--- Ejercicio14/src/main/java/Main.java
+++ Ejercicio14/src/main/java/Main.java
@@ -1,28 +1,27 @@
+import java.util.Scanner;
+
 public class Main {
 	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
 		Spotify spotify = new Spotify();
-		
-		Artista artista1 = new Artista("The Beatles", "Rock", 1000000, "Reino Unido");
-        Artista artista2 = new Artista("BTS", "K-Pop", 2000000, "Corea del Sur");
-        Artista artista3 = new Artista("Bad Bunny", "Reggaeton", 1500000, "Puerto Rico");
-        Artista artista4 = new Artista("Ariana Grande", "Pop", 3000000, "Estados Unidos");
-        Artista artista5 = new Artista("Queen", "Rock", 2500000, "Reino Unido");
-        Artista artista6 = new Artista("Rosalía", "Flamenco urbano", 1800000, "España");
-        Artista artista7 = new Artista("Enrique Iglesias", "Pop latino", 2200000, "España");
-        Artista artista8 = new Artista("Pablo Alborán", "Pop", 1700000, "España");
-        Artista artista9 = new Artista("Vanessa Martin", "Pop", 1400000, "España");
-        Artista artista10 = new Artista("Antonio Orozco", "Pop", 1300000, "España");
 
-        spotify.agregarArtista(artista1);
-        spotify.agregarArtista(artista2);
-        spotify.agregarArtista(artista3);
-        spotify.agregarArtista(artista4);
-        spotify.agregarArtista(artista5);
-        spotify.agregarArtista(artista6);
-        spotify.agregarArtista(artista7);
-        spotify.agregarArtista(artista8);
-        spotify.agregarArtista(artista9);
-        spotify.agregarArtista(artista10);
+		for (int i = 1; i <= 3; i++) {
+			System.out.println("Artista " + i + ":");
+			System.out.print("Nombre: ");
+			String nombre = scanner.nextLine();
+			System.out.print("Género: ");
+			String genero = scanner.nextLine();
+			System.out.print("Cantidad de reproducciones: ");
+			int reproducciones = scanner.nextInt();
+			scanner.nextLine(); // Consumir el salto de línea pendiente
+			System.out.print("País donde más se escucha: ");
+			String pais = scanner.nextLine();
+
+			Artista artista = new Artista(nombre, genero, reproducciones, pais);
+			spotify.agregarArtista(artista);
+		}
+
+		scanner.close();
 
 		spotify.mostrarInfo();
 		spotify.guardarArchivo();
